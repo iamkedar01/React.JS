bc2120c4226c6408c79249dfd6d426df180ee534
Kedar Dhage <kedardhage0@gmail.com>
2026-02-22 21:41:03 +0530

    Basic React

---
+++ ReactLeranEverything/Basic/Script.jsx
@@ -0,0 +1,15 @@
+function abc(){
+  Console.log("Hello world");
+}
+abc();
+
+// the Above code is written in the Java Script 
+
+// similarally we can crate write the code in the JSX
+
+function App(){
+  console.log("Welcome back");
+}
+
+//App() // this the normal method call 
+<App/> // we call the function this type also
